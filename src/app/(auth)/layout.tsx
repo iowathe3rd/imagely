68345c323a14327945d2e0c0f14ff564ab6584a6
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-07 21:04:11 +0500

    fix(auth): fixed styles add auth layout and primary color of clerk form

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from "react";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <main className="auth">{children}</main>;
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center">
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        variables: { colorPrimary: "#624cf5" },
+        variables: { colorPrimary: "#000000" },
       }}
     >
       <html lang="en">
