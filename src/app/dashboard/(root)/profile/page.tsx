c224d7fe998f312dba2ca1fe57023fa3a2b75682
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-10 01:48:56 +0500

    feat(user page): added a whole new user page

--- src/app/dashboard/(root)/profile/page.tsx
+++ src/app/dashboard/(root)/profile/page.tsx
@@ -1,5 +1,63 @@
-const ProfilePage = () => {
-  return <>Profile</>;
+import { Collection } from "@/components/shared/Collection";
+import Header from "@/components/shared/Header";
+import { getUserImages } from "@/lib/actions/image.actions";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const images = await getUserImages({ page, userId: user._id });
+
+  return (
+    <>
+      <Header title="Profile" />
+
+      <section className="mt-5 flex flex-col gap-5 sm:flex-row md:mt-8 md:gap-10">
+        <div className="w-full rounded-xl border-2 border-border bg-background p-5 shadow-lg md:px-6 md:p-8">
+          <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/icons/coins.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+
+        <div className="w-full rounded-xl border-2 border-border bg-background p-5 shadow-lg md:px-6 md:p-8">
+          <p className="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/icons/photo.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold">{images?.data.length}</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-8 md:mt-14">
+        <Collection
+          images={images?.data}
+          totalPages={images?.totalPages}
+          page={page}
+        />
+      </section>
+    </>
+  );
 };
 
 export default ProfilePage;
