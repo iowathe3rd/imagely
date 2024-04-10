95f72514fd0a74e90c68d66d93c509a76c963443
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-11 00:02:33 +0500

    feat(speed insights): added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@/components/shared/ThemeProvider';
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
@@ -43,6 +44,7 @@ export default function RootLayout({
 						{children}
 					</ThemeProvider>
 					<Analytics />
+					<SpeedInsights />
 				</body>
 			</html>
 		</ClerkProvider>
